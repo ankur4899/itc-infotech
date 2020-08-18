329e388d76a3949cd9d1622a70af6455bb406a8b
ankur.rathi <ankur.rathi@webiantechnologies.com>
2020-08-18 17:53:13 +0530

    Default route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'ITC InfoTech Application' });
+  res.send({ title: 'ITC InfoTech Application' });
 });
 
 module.exports = router;
