a12287d4250ba2d985394d4b695753e8892ffdf5
ankur.rathi <ankur.rathi@webiantechnologies.com>
2020-08-20 19:27:29 +0530

    Dockerisation of application

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     TV_URL: 'https://api.themoviedb.org/3/tv',
     API_KEY: '02fca09f7150199a4e5cd133bf752aa1',
     LANGUAGE: 'en-US',
-    DB_URL: 'mongodb://localhost:27017/itc_db'
+    DB_URL: 'mongodb://mongodb/itc_db'
 }
